561d6343167085acfcecb014bc2a53f52dbe9fe1
Luifer1994 <69922768+Luifer1994@users.noreply.github.com>
2021-02-09 19:27:15 -0500

    Update plantilla.blade.php

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -9,18 +9,15 @@
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-
+        
+      {{-- CDN sweetalert2 --}}
      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
-     <link rel="stylesheet" type="text/css" href="{{ asset('DataTables/datatables.min.css') }}"/>
-
-
+     {{-- CDN Data table --}}
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.bootstrap4.min.css">
 
-
-    <script type="text/javascript" src="{{ asset('DataTables/datatables.min.js') }}"></script>
     <title>@yield('titulo')</title>
     @yield('css')
 
